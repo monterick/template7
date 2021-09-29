d91a14fd786f4041625287845d70aecfc1f2cebc
monterick <rick_-26@hotmail.com>
2021-09-29 00:39:22 -0300

    commit

--- src/template/Menu.jsx
+++ src/template/Menu.jsx
@@ -1,13 +1,17 @@
 import './Menu.css'
 
-export default function Menu(){
 
+
+
+export default function Menu(props){
+    
+   
     return(
         <div className="menu">
-          <div className="menu-content" >
+          <div className="menu-content"  style={{display: props.visu}}>
               <div className="row">
                   <div style={{color:"white",fontSize:"25px",textAlign:"right",margin:"10px"}} className="col pointer">
-                      X
+                      {props.children}
                   </div>
               </div>
               <div style={{textAlign:"center"}}>
@@ -17,6 +21,11 @@ export default function Menu(){
                   <li><a href="#">Page 3</a></li>
                   <li><a href="#">Page 4</a></li>
                   <li><a href="#">Page 5</a></li>
+                  <li><a href="#">Page 6</a></li>
+                  <li><a href="#">Page 7</a></li>
+                  <li><a href="#">Page 8</a></li>
+                  <li><a href="#">Page 9</a></li>
+                  <li><a href="#">Page 10</a></li>
               </ul>
               </div>
           </div>
